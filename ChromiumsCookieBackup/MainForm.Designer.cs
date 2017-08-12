21a76bba0c7ada9cc02ec300ff84e668e9834dbf
Flavio Moreira <flavio.moreira@outlook.com>
2017-08-12 16:23:38 -0300

    Chromium's Cookie Backup

---
+++ ChromiumsCookieBackup/MainForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace ChromeCookieBackup
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.FolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.RestoreButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.folderBrowse = new System.Windows.Forms.Button();
+            this.UserDataTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FolderBrowser
+            // 
+            this.FolderBrowser.HelpRequest += new System.EventHandler(this.FolderBrowser_HelpRequest);
+            // 
+            // RestoreButton
+            // 
+            this.RestoreButton.Location = new System.Drawing.Point(345, 54);
+            this.RestoreButton.Name = "RestoreButton";
+            this.RestoreButton.Size = new System.Drawing.Size(88, 26);
+            this.RestoreButton.TabIndex = 19;
+            this.RestoreButton.Text = "Restaurar";
+            this.RestoreButton.UseVisualStyleBackColor = true;
+            this.RestoreButton.Click += new System.EventHandler(this.RestoreButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(251, 54);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(88, 26);
+            this.SaveButton.TabIndex = 16;
+            this.SaveButton.Text = "Backup";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.BackupButton_Click);
+            // 
+            // folderBrowse
+            // 
+            this.folderBrowse.Location = new System.Drawing.Point(408, 28);
+            this.folderBrowse.Name = "folderBrowse";
+            this.folderBrowse.Size = new System.Drawing.Size(24, 20);
+            this.folderBrowse.TabIndex = 15;
+            this.folderBrowse.Text = "...";
+            this.folderBrowse.UseVisualStyleBackColor = true;
+            this.folderBrowse.Click += new System.EventHandler(this.FolderBrowse_Click);
+            // 
+            // UserDataTextBox
+            // 
+            this.UserDataTextBox.Location = new System.Drawing.Point(17, 28);
+            this.UserDataTextBox.Name = "UserDataTextBox";
+            this.UserDataTextBox.Size = new System.Drawing.Size(385, 20);
+            this.UserDataTextBox.TabIndex = 14;
+            this.UserDataTextBox.TextChanged += new System.EventHandler(this.UserDataTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Dados de Usuário";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 83);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.statusStrip1.Size = new System.Drawing.Size(447, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 20;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel1.Text = "  ";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.statusLabel.Size = new System.Drawing.Size(64, 17);
+            this.statusLabel.Text = "Status: Idle";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "*.db";
+            this.openFileDialog.InitialDirectory = "Desktop";
+            this.openFileDialog.Title = "Choose backup file...";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(17, 54);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(98, 17);
+            this.radioButton1.TabIndex = 21;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Google Chrome";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(121, 54);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 17);
+            this.radioButton2.TabIndex = 21;
+            this.radioButton2.Text = "Vivaldi";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(304, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Provided by Flavio Moreira";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 105);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.RestoreButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.folderBrowse);
+            this.Controls.Add(this.UserDataTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Chromium\'s Cookie Backup";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.FolderBrowserDialog FolderBrowser;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog;
+		private System.Windows.Forms.Button RestoreButton;
+		private System.Windows.Forms.Button SaveButton;
+		private System.Windows.Forms.Button folderBrowse;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog;
+		public System.Windows.Forms.TextBox UserDataTextBox;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
